bdeb70a1559332c50bb4fa9a07bcf6bba4d87e80
Jason Schulz <jason@schulz.studio>
2024-04-01 10:42:24 -0400

    removing solid.js references

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
-import solidJs from "@astrojs/solid-js";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), solidJs()]
+  integrations: [react()],
   // site: 'https://jasonpaulso.github.io',
   // base: '/homepage_2024',
   // trailingSlash: 'never',
